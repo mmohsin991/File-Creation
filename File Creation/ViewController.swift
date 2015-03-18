96257b93cf6e5c6bb0a4b74e545d1b60ee857c67
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-03-18 18:28:10 +0500

    upload
    swift

--- File Creation/ViewController.swift
+++ File Creation/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        var docPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as NSString
+        
+        let isCreat: Bool =  NSFileManager.defaultManager().createDirectoryAtPath(docPath.stringByAppendingPathComponent("MohsinFolder1"), withIntermediateDirectories: true, attributes: nil, error: nil)
+        
+        if isCreat {
+            
+            println("all folder: \(NSFileManager.defaultManager().contentsOfDirectoryAtPath(docPath, error: nil))")
+            
+        }
+        
+        println(docPath)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func click(sender: AnyObject) {
+        
+        var task = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: "http://images4.fanpop.com/image/photos/24100000/Roses-from-Princess-yorkshire_rose-24169767-2104-1860.jpg")! ){(data, response, error) in
+            
+            println("Response: \(response)")
+            
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            
+          //  println("Body: \(data)\n\n")
+        }
+        task.resume()
+    }
 
 }
 
